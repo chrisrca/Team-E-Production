815b33b84489b3d0880bd11d141fa4af63b92d1b
Kai Davidson <>
2024-03-30 22:02:48 -0400

    Massive commit, database code added, prisma version stays the same. Still need to integrate into app and perhaps abstract to backed.

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,7 +2,6 @@ import createError, { HttpError } from "http-errors";
 import express, { Express, NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import exampleRouter from "./routes/example.ts";
 
 const app: Express = express(); // Setup the backend
 
@@ -21,7 +20,6 @@ app.use(cookieParser()); // Cookie parser
 
 // Setup routers. ALL ROUTERS MUST use /api as a start point, or they
 // won't be reached by the default proxy and prod setup
-app.use("/api/high-score", exampleRouter);
 app.use("/healthcheck", (req, res) => {
   res.status(200).send();
 });
