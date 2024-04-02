57d0a701077b527ff67491f54eb101b99e40ff71
Marc <marcw5450@gmail.com>
2024-04-01 20:03:19 -0400

    New routing and component for displaying nodes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import FlowerService from "@/components/FlowerServiceRequest.tsx";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Hamburger from "./components/Hamburger.tsx";
 import MapPage from "@/components/MapPage.tsx";
+import ViewNodes from "@/components/ViewNodes.tsx";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/map" element={<MapPage />} />
           <Route path="/flower-service" element={<FlowerService />} />
+          <Route path="/nodes" element={<ViewNodes />} />
         </Routes>
       </BrowserRouter>
     </div>
