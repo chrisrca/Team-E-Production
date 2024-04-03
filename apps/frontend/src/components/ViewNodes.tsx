556a3ddeb7bd94817fbdccbaf21a2ab57f9312d7
Kai Davidson <>
2024-04-03 13:21:06 -0400

    Marc changes to ViewNodes, much better styling. This is terrible to commit on main but must be done.

--- apps/frontend/src/components/ViewNodes.tsx
+++ apps/frontend/src/components/ViewNodes.tsx
@@ -4,18 +4,19 @@ export default function ViewNodes(inputData: { data: DBNode[] | Edge[] }) {
     const data = inputData.data;
     if (data.length != 0) {
         return (
-            <div className="pt-5 p-5 relative flex flex-col items-center justify-center">
+            <div className="relative overflow-x-auto flex flex-col items-center justify-center">
                 <h1 className="text-3xl font-bold">Data</h1>
-                <table className="border-collapse border-spacing-2 border border-slate-400">
-                    <thead>
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             {Object.keys(data[0]).map((value, index) => (
-                                <td
+                                <th
                                     key={index}
-                                    className="border border-primary p-1"
+                                    scope="col"
+                                    className="px-6 py-3"
                                 >
                                     {value}
-                                </td>
+                                </th>
                             ))}
                         </tr>
                     </thead>
@@ -23,14 +24,11 @@ export default function ViewNodes(inputData: { data: DBNode[] | Edge[] }) {
                         {data.map((currentValue, index) => (
                             <tr
                                 key={index}
-                                className="border border-primary p-1"
+                                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                             >
                                 {Object.values(currentValue).map(
                                     (value, index) => (
-                                        <td
-                                            key={index}
-                                            className="border border-primary p-1"
-                                        >
+                                        <td key={index} className="px-6 py-4">
                                             {value}
                                         </td>
                                     ),
