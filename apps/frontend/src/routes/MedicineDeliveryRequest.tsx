fb34a844bef78cf85fcdf58956d2427016a62bd4
Nexus1801 <ubervenx@gmail.com>
2024-04-04 17:01:34 -0400

    medicine request updated with tailwind

--- apps/frontend/src/routes/MedicineDeliveryRequest.tsx
+++ apps/frontend/src/routes/MedicineDeliveryRequest.tsx
@@ -1,8 +1,9 @@
 import { ChangeEvent, FormEvent, useState } from "react";
 import { FormInput } from "@/components/ui/formInput.tsx";
-import tylenol from "../images/tylenol.jpg";
+import axios from "axios";
+/*import tylenol from "../images/tylenol.jpg";
 import advil from "../images/advil.jpg";
-import melatonin from "../images/melatonin.jpg";
+import melatonin from "../images/melatonin.jpg";*/
 
 interface DrugDeliveryData {
     patientName: string;
@@ -12,16 +13,15 @@ interface DrugDeliveryData {
     drugQuantity: string;
 }
 
+async function sendMedicineRequest(drugOrder: DrugDeliveryData) {
+    axios.post("/api/drugs", drugOrder).then((res) => {
+        console.log(res);
+    });
+}
+
 export default function DrugDelivery() {
     const drugOrderData = [];
-    const [displayedDrugData, setDisplayedDrugData] =
-        useState<DrugDeliveryData>({
-            patientName: "",
-            roomNumber: "",
-            patientCondition: "",
-            drugName: "",
-            drugQuantity: "",
-        });
+
     const [drugOrder, setDrugOrder] = useState<DrugDeliveryData>({
         patientName: "",
         roomNumber: "",
@@ -33,7 +33,12 @@ export default function DrugDelivery() {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         drugOrderData.push(drugOrder);
-        setDisplayedDrugData(drugOrder);
+
+        if (drugOrder.patientName === "" || drugOrder.roomNumber === "") {
+            alert("Please fill out the required fields");
+            return;
+        }
+        sendMedicineRequest(drugOrder);
 
         setDrugOrder({
             patientName: "",
@@ -44,7 +49,7 @@ export default function DrugDelivery() {
         });
     };
 
-    const renderDrugImage = () => {
+    /*const renderDrugImage = () => {
         switch (drugOrder.drugName) {
             case "Tylenol - $5":
                 return (
@@ -71,10 +76,13 @@ export default function DrugDelivery() {
                     />
                 );
         }
-    };
+    };*/
 
     return (
-        <>
+        <div className="flex flex-col flex-auto justify-center items-center h-screen">
+            <h1 className="text-extrabold text-2xl p-10">
+                Medicine Delivery Request Form
+            </h1>
             <form onSubmit={handleSubmit} className="w-full max-w-lg">
                 <div className="flex flex-wrap -mx-3 mb-6">
                     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -165,7 +173,7 @@ export default function DrugDelivery() {
                         />
                     </div>
                 </div>
-                {renderDrugImage()};
+
                 <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                     <button
                         className="bg-blue-900 hover:bg-transparent text-white font-semibold hover:text-blue-900 py-2.5 px-4 border hover:border-blue-900 rounded hover:rounded-none"
@@ -175,7 +183,6 @@ export default function DrugDelivery() {
                     </button>
                 </div>
             </form>
-            <p>Patient Name: {displayedDrugData.patientName}</p>
-        </>
+        </div>
     );
 }
