b7d7d9c44529fc3c2f804708c84237466349c8cc
Cakhr <cgwilliams3@wpi.edu>
2024-04-08 18:18:12 -0400

    working(??) yarn is pitching a fit

--- apps/frontend/src/routes/ServiceRequests.tsx
+++ apps/frontend/src/routes/ServiceRequests.tsx
@@ -1,21 +1,22 @@
 "use client";
 import * as React from "react";
-import { useState } from "react";
+import { SubmitEvent, ChangeEvent } from "react";
+//import { SubmitEvent } from "react";
 // import { zodResolver } from "@hookform/resolvers/zod";
 // import { useForm } from "react-hook-form";
 // import { z } from "zod";
 // import axios from "axios";
 // import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-// import {
+//import {
 //     Form,
 //     FormControl,
 //     FormDescription,
 //     FormField,
 //     FormItem,
-//     FormLabel,
+//    FormLabel,
 //     FormMessage,
-// } from "@/components/ui/form";
+//} from "@/components/ui/form";
 
 import {
     Select,
@@ -26,6 +27,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
+//import FormInput from "@/components/ui/formInput";
 
 // import {
 //     Table,
@@ -42,6 +44,7 @@ type Component = {
     title: string; //Element title
     placeholder: string; //Displayed placeholder text
     required: boolean; //Required field?
+    id: number;
 };
 
 type FormSelect = Component & {
@@ -49,123 +52,203 @@ type FormSelect = Component & {
     options: string[]; //Displayed options
 };
 
-type Submission = {
-    data: string[];
-};
+// type TableData = {
+//     submission: string[];
+// };
 
-const test = [
-    {
-        type: "text",
-        title: "text 1",
-        placeholder: "placeholder 1",
-        required: true,
-    },
-    {
-        type: "select",
-        title: "select 1",
-        placeholder: "select placeholder 1",
-        required: false,
-        label: "Options",
-        options: ["option 1", "option 2", "option 3"],
-    },
-];
+// type Submission = {
+//     data: string[];
+// };
 
-const makeForm = (props: Component[]) => {
-    return (
-        <>
-            <div className="w-[35rem] m-auto p-10">{mapElements(props)}</div>
-        </>
-    );
-};
+// type formSchema = {
+//     field1: string;
+//     field2: string;
+//     field3: string;
+// };
 
-const mapElements = (props: Component[]) => {
-    console.log(props);
-    return props.map(identifyComponent);
-};
-
-const identifyComponent = (props: Component) => {
-    if (props.type.includes("text")) {
-        console.log(props.title);
-        console.log("component == text");
-        return inputComp(props);
-    } else if (props.type.includes("select")) {
-        console.log(props.title);
-        console.log("component == select");
-        return selectComp(props as FormSelect);
-    } else {
-        console.error("Failed to identify element - " + props.type);
-    }
-};
-const inputComp = (props: Component) => {
-    console.log("making input element");
-    return (
-        <>
-            <Input type={props.title} placeholder={props.placeholder} />
-        </>
-    );
-};
-
-const selectComp = (props: FormSelect) => {
-    return (
-        <>
-            <Select>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder={props.placeholder} />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>{props.label}</SelectLabel>
-                        {props.options.map((value) => (
-                            <SelectItem value={value.toString()}>
-                                {value}
-                            </SelectItem>
-                        ))}
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        </>
-    );
-};
-
-// const formSchema = z.object({
-//     employeeName: z.string().min(1, {
-//         message: "Please specify Employee name.",
-//     }),
-//     location: z.string().min(1, {
-//         message: "Please specify a location.",
-//     }),
-//     message: z.string(),
-//     gift: z.string().min(1, {
-//         message: "Please select a gift.",
-//     }),
-//     priority: z.string(),
-//     status: z.string(),
-//     anonymous: z.boolean(),
-// });
+// const tableComp = (props: TableData) => {
+//     return (
+//         <div className="p-10 m-auto w-min">
+//             <Table>
+//                 <TableCaption>List of requests.</TableCaption>
+//                 <TableHeader>
+//                     <TableRow>
+//                         <TableHead>Employee Name</TableHead>
+//                         <TableHead>Location</TableHead>
+//                         <TableHead>Message</TableHead>
+//                         <TableHead>Gift</TableHead>
+//                         <TableHead>Priority</TableHead>
+//                         <TableHead>Status</TableHead>
+//                         <TableHead>Anonymous</TableHead>
+//                     </TableRow>
+//                 </TableHeader>
+//                 <TableBody>
+//                     {submissions.map((request) => (
+//                         <TableRow key={request.toString()}>
+//                             <TableCell>{request.employeeName}</TableCell>
+//                             <TableCell>{request.location}</TableCell>
+//                             <TableCell>{request.message}</TableCell>
+//                             <TableCell>{request.gift}</TableCell>
+//                             <TableCell>{request.priority}</TableCell>
+//                             <TableCell>{request.status}</TableCell>
+//                             <TableCell>{request.anonymous.toString()}</TableCell>
+//                         </TableRow>
+//                     ))}
+//                 </TableBody>
+//             </Table>
+//         </div>
+//     )
+// }
 
 export const ServiceRequests = () => {
-    const [submissions, setSubmissions] = useState(new Array<Submission>());
-    // const form = useForm<z.infer<typeof formSchema>>({
-    //     resolver: zodResolver(formSchema),
-    //     defaultValues: {
-    //         employeeName: "",
-    //         location: "",
-    //         message: "",
-    //         gift: "",
-    //         priority: "",
-    //         status: "",
-    //         anonymous: false,
-    //     },
-    // });
-    //
-    // // 2. Define a submit handler.
-    function onSubmit(values: Submission) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values);
-        setSubmissions([...submissions, values]);
-    }
-    onSubmit;
+    // Form Generation
 
-    return <>{makeForm(test)}</>;
+    const makeForm = (props: FormSelect[] | Component[]) => {
+        return (
+            <>
+                <div className="flex flex-col flex-auto w-[35rem] m-auto p-10 space-y-8 items-left">
+                    {props.map(identifyComponent)}
+                </div>
+            </>
+        );
+    };
+
+    const identifyComponent = (props: Component) => {
+        if (props.type.includes("text")) {
+            // console.log(props.title);
+            // console.log("component == text");
+            return inputComp(props);
+        } else if (props.type.includes("select")) {
+            // console.log(props.title);
+            // console.log("component == select");
+            return selectComp(props as FormSelect);
+        } else {
+            console.error("Failed to identify element - " + props.type);
+        }
+    };
+
+    //Beginning of Elements
+
+    const inputComp = (props: Component) => {
+        console.log("making input element");
+        return (
+            <>
+                <div className="">
+                    <label className="block uppercase tracking-wide text-foreground text-xs font-bold mb-2">
+                        {props.title}
+                    </label>
+                    <Input
+                        type={props.title}
+                        placeholder={props.placeholder}
+                        onChange={(e: ChangeEvent) =>
+                            (serviceReqArr[props.id] = e.target.value)
+                        }
+                    />
+                </div>
+            </>
+        );
+    };
+    const selectComp = (props: FormSelect) => {
+        return (
+            <>
+                <div className="">
+                    <label className="block uppercase tracking-wide text-foreground text-xs font-bold mb-2">
+                        {props.title}
+                    </label>
+                    <Select
+                        onValueChange={(value: string) =>
+                            (serviceReqArr[props.id] = value)
+                        }
+                        required={props.required}
+                    >
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder={props.placeholder} />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectLabel>{props.label}</SelectLabel>
+                                {props.options.map((option) => (
+                                    <SelectItem value={option.toString()}>
+                                        {option}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </div>
+            </>
+        );
+    };
+
+    //End of Elements
+
+    //const serviceRequestData = [[]];
+
+    const test = [
+        {
+            type: "text",
+            title: "text 1",
+            placeholder: "text placeholder 1",
+            required: true,
+            id: null,
+        },
+        {
+            type: "select",
+            title: "select 1",
+            placeholder: "select placeholder 1",
+            required: false,
+            id: null,
+            label: "Options",
+            options: ["option 1", "option 2", "option 3"],
+        },
+        {
+            type: "text",
+            title: "text 2",
+            placeholder: "text placeholder 2",
+            required: true,
+            id: null,
+        },
+    ];
+
+    const serviceReqArr = [""];
+    const serviceReqData = [[""]];
+
+    const serviceReq = () => {
+        test.map((field) => (field.id = test.indexOf(field)));
+        console.log(test);
+    };
+
+    serviceReq();
+    console.log(serviceReqArr);
+
+    // const [serviceReq, setServiceRequest] = useState<formSchema>({
+    //
+    //
+    // });
+
+    const handleSubmit = (e: SubmitEvent) => {
+        e.preventDefault();
+        //Saving of data
+        serviceReqData.push(serviceReqArr);
+
+        console.log(serviceReqArr);
+        //Clearing of form
+        serviceReqArr.fill("");
+    };
+
+    return (
+        <>
+            <div className="flex flex-col space-y-8 items-center bg-secondary w-[35rem] rounded m-auto ">
+                <form className="rounded" onSubmit={handleSubmit}>
+                    {makeForm(test)}
+                    <button
+                        className="bg-blue-900 hover:bg-accent text-white font-semibold hover:text-blue-900 py-2.5 px-4 border hover:border-blue-900 rounded"
+                        type={"submit"}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </>
+    );
 };
