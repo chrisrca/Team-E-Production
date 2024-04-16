60cd5471f72610bf01a3606e6db6703ade046d7c
lorenzomanfredi <lorims@me.com>
2024-04-16 12:34:41 -0400

    avatar change

--- apps/frontend/src/components/UserArea.tsx
+++ apps/frontend/src/components/UserArea.tsx
@@ -57,8 +57,8 @@ export default function UserArea() {
                     {isAuthenticated ? (
                         <Avatar>
                             <AvatarImage
-                                src="https://github.com/shadcn.png"
-                                alt="user"
+                                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                                alt="user avatar"
                             />
                             <AvatarFallback>CN</AvatarFallback>
                         </Avatar>
