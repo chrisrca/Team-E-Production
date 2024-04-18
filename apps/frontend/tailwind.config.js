dad1e84e63f4c2957cdd07404eed30b081d918b0
Kai Davidson <>
2024-04-17 22:25:59 -0400

    Working on debugging AWS tailwind issues

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    important: true,
     darkMode: ["class"],
     content: [
+        "./index.html"
         "./routes/**/*.{ts,tsx}",
         "./components/**/*.{ts,tsx}",
         "./app/**/*.{ts,tsx}",
