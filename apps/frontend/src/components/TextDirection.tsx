cadfcda65e224214ff8bc239748ecd1417fa3ef4
Marc <marcw5450@gmail.com>
2024-04-22 17:44:49 -0400

    function when on same floor and building

--- apps/frontend/src/components/TextDirection.tsx
+++ apps/frontend/src/components/TextDirection.tsx
@@ -1,31 +1,55 @@
 import { DBNode } from "common/src/types";
 
 export default function TextDirection(nodes: DBNode[]) {
-    console.log(nodes);
+    const directions=["Head towards " + nodes[1].shortName];
 
-    const directions: string[] = ["Start"];
+    let deltaX = nodes[1].xcoord - nodes[0].xcoord;
+    let deltaY = nodes[1].ycoord - nodes[0].ycoord;
 
-    for (let i = 0; i < nodes.length; i++) {
+    let absoluteAngle = (Math.atan2(deltaY, deltaX) * 180) / Math.PI; // degrees
+    let relativeAngle = 0; // indicates rotation by x amount [degrees]
+    let prevAngle = 0; // previous absolute angle
+
+    for (let i = 1; i < nodes.length; i++) {
+
         const currNode = nodes[i];
         const nextNode = nodes[i + 1];
+
+        if(!currNode || !nextNode) break;
 
         if (
             currNode.floor === nextNode.floor &&
             currNode.building === nextNode.building
         ) {
-            // same floor and building
-            const deltaX = nextNode.xcoord - currNode.xcoord;
-            const deltaY = nextNode.ycoord - currNode.ycoord;
-            const angle = (Math.atan2(deltaY,deltaX))*180/Math.PI; // degrees
+            // if in the same building and floor
+            deltaX = nextNode.xcoord - currNode.xcoord;
+            deltaY = nextNode.ycoord - currNode.ycoord;
 
-            if(Math.abs(angle) < 45) directions.push("Right"); // right turn if -45<angle<45
-            else if((180 - Math.abs(angle)) < 45) directions.push("Left"); // left turn if angle between -135 and 135 on unit circle
-            else if(Math.abs(angle) === 45) directions.push("Slight Right");
-            else if(Math.abs(angle) === 135) directions.push("Slight Left");
-            else directions.push("Straight"); // +/- 90 deg considered straight
+            absoluteAngle = (Math.atan2(deltaY, deltaX) * 180) / Math.PI; // degrees
 
+            relativeAngle = prevAngle - absoluteAngle; // indicates rotation direction and amount
+
+            const SlightRight = (-30>=relativeAngle && relativeAngle>=-60);
+            const Right = (-60>relativeAngle && relativeAngle>-120);
+            const SharpRight = (-120>=relativeAngle && relativeAngle>=-150);
+            // Straight -150 +/-180 150
+            const SharpLeft = (120<=relativeAngle && relativeAngle<=150);
+            const Left = (60<relativeAngle && relativeAngle<120);
+            const SlightLeft = (30<=relativeAngle && relativeAngle<=60);
+
+            if(SlightRight) directions.push(" Slight Right ");
+            else if(Right) directions.push(" Right ");
+            else if(SharpRight) directions.push(" Sharp Right ");
+            else if(SharpLeft) directions.push(" Sharp Left ");
+            else if(Left) directions.push(" Left ");
+            else if(SlightLeft) directions.push(" Slight Left ");
+            else directions.push(` Straight `);
+
+            prevAngle = absoluteAngle;
         }
     }
 
-    return directions; // change later
+    return directions;
 }
+
+
