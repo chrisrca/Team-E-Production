5ba2a6f673d5aabc9b9c6ffaf1dca771d68b4323
brandonyeu <brandonyeu8@gmail.com>
2024-04-22 20:59:16 -0400

    all done ready to merge

--- apps/frontend/src/routes/WelcomePage.tsx
+++ apps/frontend/src/routes/WelcomePage.tsx
@@ -37,7 +37,7 @@ export default function WelcomePage() {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
     const handleLogin = () => {
         loginWithRedirect({
-            appState: {returnTo: "/home"}
+            appState: { returnTo: "/home" },
         });
     };
 
