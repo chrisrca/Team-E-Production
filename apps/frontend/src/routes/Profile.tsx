5f1466f68467a3f25b2bd76cc524478ee47f7b5a
ajensen544 <akselgradyjensen@gmail.com>
2024-04-24 21:46:28 -0400

    Profile Page should be connected to employees individually assigned services

--- apps/frontend/src/routes/Profile.tsx
+++ apps/frontend/src/routes/Profile.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {useAuth0, User} from "@auth0/auth0-react";
+import {FlowerServiceRequest} from "common/src/types";
+import { ViewNodes } from "@/components";
 
 function ProfilePage() {
-    const serviceRequests = [
-        {
-            id: 1,
-            date: "2023-04-14",
-            type: "Flower Delivery Service",
-            status: "Completed",
-        },
-        {
-            id: 2,
-            date: "2023-04-20",
-            type: "Security Request",
-            status: "In Progress",
-        },
-    ];
+    const { user } = useAuth0();
+    const [userInfo, setUserInfo] = useState<User | null>(null);
+    const [serviceRequests, setServiceRequests] = useState<FlowerServiceRequest>([]);
+    const [employeeName, setEmployeeName] = useState("");
+
+    useEffect(() => {
+        if (user) {
+            setUserInfo(user);
+            if (user.name) {
+                setEmployeeName(user.name);
+            }
+        }
+        const fetchServiceRequests = async () => {
+            try {
+                const response = await axios.get("`/api/service-requests/${employeeName}`");
+                setServiceRequests(response.data);
+            } catch (error) {
+                console.error(error);
+                console.log(userInfo);
+            }
+        };
+
+        fetchServiceRequests();
+    }, [employeeName, user]);
 
     return (
         <div className="container mx-auto p-4">
@@ -22,41 +36,7 @@ function ProfilePage() {
                 <h1 className="text-xl font-bold mt-12 m-4">User Profile</h1>
                 <p className="text-gray-700">Wong Man | adminD24E@gmail.com</p>
             </div>
-            <div className="bg-white shadow-md rounded-lg p-4">
-                <h2 className="text-lg font-semibold">
-                    Service Request History
-                </h2>
-                <table className="min-w-full leading-normal">
-                    <thead>
-                        <tr>
-                            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Date
-                            </th>
-                            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Service Type
-                            </th>
-                            <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Status
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {serviceRequests.map((request) => (
-                            <tr key={request.id}>
-                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    {request.date}
-                                </td>
-                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    {request.type}
-                                </td>
-                                <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    {request.status}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <ViewNodes data = {serviceRequests}/>
         </div>
     );
 }
