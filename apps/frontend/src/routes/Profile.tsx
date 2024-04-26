e1d6047a35e0bad07fe901e2096d22f0c15acba5
ajensen544 <akselgradyjensen@gmail.com>
2024-04-26 16:12:51 -0400

    data viewer for employee services on profile page

--- apps/frontend/src/routes/Profile.tsx
+++ apps/frontend/src/routes/Profile.tsx
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {useAuth0, User} from "@auth0/auth0-react";
-import {FlowerServiceRequest} from "common/src/types";
+import { FlowerServiceRequest } from "common/src/types";
 import { ViewNodes } from "@/components";
 
 function ProfilePage() {
     const { user } = useAuth0();
     const [userInfo, setUserInfo] = useState<User | null>(null);
-    const [serviceRequests, setServiceRequests] = useState<FlowerServiceRequest>([]);
+    const [serviceRequests, setServiceRequests] = useState<FlowerServiceRequest[]>([]);
     const [employeeName, setEmployeeName] = useState("");
 
     useEffect(() => {
@@ -19,7 +19,7 @@ function ProfilePage() {
         }
         const fetchServiceRequests = async () => {
             try {
-                const response = await axios.get("`/api/service-requests/${employeeName}`");
+                const response = await axios.get(`/api/service-requests/${employeeName}`);
                 setServiceRequests(response.data);
             } catch (error) {
                 console.error(error);
@@ -28,13 +28,13 @@ function ProfilePage() {
         };
 
         fetchServiceRequests();
-    }, [employeeName, user]);
+    }, [userInfo, employeeName, user]);
 
-    return (
+    if (userInfo) return (
         <div className="container mx-auto p-4">
             <div className="mb-8">
                 <h1 className="text-xl font-bold mt-12 m-4">User Profile</h1>
-                <p className="text-gray-700">Wong Man | adminD24E@gmail.com</p>
+                <p className="text-gray-700"> { employeeName } </p>
             </div>
             <ViewNodes data = {serviceRequests}/>
         </div>
